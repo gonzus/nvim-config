deb8da602d904d6c9370c527bf0e556af8e61010
Gonzalo Diethelm <gonzo@fb.com>
2022-09-13 16:59:57 +0200

    Refactor illuminate code

--- lua/gonzo/lsp/handlers.lua
+++ lua/gonzo/lsp/handlers.lua
@@ -29,22 +29,39 @@ local function lsp_keymaps(bufnr)
   keymap(bufnr, "n", "<leader>lq", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
 end
 
-M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+local function lsp_highlight_document(client)
+  -- Set autocommands conditional on server_capabilities
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
+      augroup lsp_document_highlight
+        autocmd! * <buffer>
+        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+      augroup END
+    ]] ,
+      false
+    )
   end
+end
 
-  if client.name == "sumneko_lua" then
-    client.resolved_capabilities.document_formatting = false
-  end
-
-  lsp_keymaps(bufnr)
+local function illuminate_client(client)
   local status_ok, illuminate = pcall(require, "illuminate")
   if not status_ok then
     vim.notify("Could not require module 'illuminate'")
     return
   end
   illuminate.on_attach(client)
+end
+
+M.on_attach = function(client, bufnr)
+  -- if client.name == "sumneko_lua" then
+  --   client.resolved_capabilities.document_formatting = false
+  -- end
+
+  lsp_keymaps(bufnr)
+  lsp_highlight_document(client)
+  illuminate_client(client)
 end
 
 return M
