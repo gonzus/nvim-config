6c372c507a6cdab248f234ee45fd244ea8eb6f9d
Gonzalo Diethelm <gonzo@fb.com>
2022-09-15 15:22:46 +0200

    Cleanup lualine for inactive buffers

--- lua/gonzo/lualine.lua
+++ lua/gonzo/lualine.lua
@@ -5,54 +5,54 @@ if not status_ok then
 end
 
 local diagnostics = {
-	'diagnostics',
-	sources = { 'nvim_diagnostic' },
-	sections = { 'error', 'warn' },
-	symbols = { error = ' ', warn = ' ' },
-	colored = false,
-	update_in_insert = false,
-	always_visible = true,
+  'diagnostics',
+  sources = { 'nvim_diagnostic' },
+  sections = { 'error', 'warn' },
+  symbols = { error = ' ', warn = ' ' },
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
 }
 
 local filetype = {
-	'filetype',
-	icons_enabled = false,
-	icon = nil,
+  'filetype',
+  icons_enabled = false,
+  icon = nil,
 }
 
 local branch = {
-	'branch',
-	icons_enabled = true,
-	icon = '',
+  'branch',
+  icons_enabled = true,
+  icon = '',
 }
 
 local location = {
-	'location',
-	padding = 0,
+  'location',
+  padding = 0,
 }
 
 local options = {
-	options = {
+  options = {
     theme = 'nord',
-		component_separators = '',
-		section_separators = '',
-		disabled_filetypes = { 'dashboard', 'NvimTree', 'Outline' },
-	},
-	sections = {
-		lualine_a = { 'filename' },
-		lualine_b = { branch },
-		lualine_c = { diagnostics },
+    component_separators = '',
+    section_separators = '',
+    disabled_filetypes = { 'dashboard', 'NvimTree', 'Outline' },
+  },
+  sections = {
+    lualine_a = { 'filename' },
+    lualine_b = { branch },
+    lualine_c = { diagnostics },
     lualine_x = { 'encoding', filetype },
-		lualine_y = { 'progress' },
-		lualine_z = { location },
-	},
-	inactive_sections = {
-		lualine_a = { 'filename' },
-		lualine_b = {},
-		lualine_c = {},
-    lualine_x = { 'encoding', filetype },
-		lualine_y = { 'progress' },
-		lualine_z = { location },
-	},
+    lualine_y = { 'progress' },
+    lualine_z = { location },
+  },
+  inactive_sections = {
+    lualine_a = { 'filename' },
+    lualine_b = {},
+    lualine_c = {},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = { location },
+  },
 }
 lualine.setup(options)
